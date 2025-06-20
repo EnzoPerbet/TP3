6eb7785251bb3ac173237226b25d6dbebd50acf0
katastrophe <72570393+Katastrophique@users.noreply.github.com>
2025-06-20 16:31:42 +0200

    test

--- tests/test_app_web.py
+++ tests/test_app_web.py
@@ -4,18 +4,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_division():
-    if sys.platform.startswith("win"):
-        service = Service("drivers/chromedriver.exe")
-    else:
-        service = Service("drivers/chromedriver")
-
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get("http://localhost:5000")
@@ -38,16 +35,12 @@ def test_division():
     driver.quit()
 
 def test_division_par_zero():
-    if sys.platform.startswith("win"):
-        service = Service("drivers/chromedriver.exe")
-    else:
-        service = Service("drivers/chromedriver")
-
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
+    service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
 
     driver.get("http://localhost:5000")
